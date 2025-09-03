edddebd79787e6b6f5e0c1e69bd452321f2ff1cf
Dmitry Pack <dmitrypack0@gmail.com>
2025-09-03 13:41:29 +0300

    first commit

---
+++ logistic/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+
+from logistic.models import Product, StockProduct, Stock
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description']
+
+
+class ProductPositionSerializer(serializers.ModelSerializer):
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
+
+
+class StockSerializer(serializers.ModelSerializer):
+    positions = ProductPositionSerializer(many=True)
+
+    class Meta:
+        model = Stock
+        fields = ['id', 'address', 'positions']
+
+    def create(self, validated_data):
+        positions = validated_data.pop('positions')
+        stock = super().create(validated_data)
+        for pos_data in positions:
+            StockProduct.objects.create(stock=stock, **pos_data)
+        return stock
+
+
+    def update(self, instance, validated_data):
+        positions = validated_data.pop('positions')
+        instance.address = validated_data.get('address', instance.address)
+        instance.save()
+
+
+        instance.positions.all().delete()
+        for pos_data in positions:
+            StockProduct.objects.create(stock=instance, **pos_data)
+
---
+++ logistic/views.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+
+
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'description']
+    # при необходимости добавьте параметры фильтрации
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['products']
+    # при необходимости добавьте параметры фильтрации
