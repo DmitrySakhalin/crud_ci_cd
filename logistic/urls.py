b4d465554bea4dd5ce0e91ee30fa6a98900113fa
Dmitry Pack <dmitrypack0@gmail.com>
2025-09-25 11:15:06 +0300

    rw ci.yml test-9

--- logistic/urls.py
+++ logistic/urls.py
@@ -1,4 +1,6 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
+from django.http import JsonResponse
 
 from logistic.views import ProductViewSet, StockViewSet
 
@@ -6,4 +8,9 @@ router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
 
-urlpatterns = router.urls
+def test_view(request):
+    return JsonResponse({'ok': True})
+
+urlpatterns = router.urls + [
+    path('test/', test_view),
+]
